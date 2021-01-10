dd56e2b4df9388c82e542b03b391f6444145846d
bharath2212 <bharath.janjanam98@gmail.com>
2021-01-10 15:49:37 +0530

    commit

--- imports/ui/Time.jsx
+++ imports/ui/Time.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
-export const Time = (props) => {
+export const Time = () => {
   return (
     
 <div className='time'>
-        <h6>{props} </h6>
+        <h6>time </h6>
     </div>
   );
 };
